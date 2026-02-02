afe1b1b51998497aa03bd78b385e19dff6c1f25f
zkkython <zkkython@gmail.com>
2026-02-02 20:11:02 +0800

    remove the old logic;

--- src/models/__init__.py
+++
@@ -1,9 +0,0 @@
-import importlib
-import os
-import pkgutil
-
-# 自动发现并导入当前目录下所有的 .py 模块，从而触发 @ModelRegistry.register
-pkg_dir = os.path.dirname(__file__)
-for _, module_name, _ in pkgutil.iter_modules([pkg_dir]):
-    if module_name not in ["base", "model"]:  # 排除基类和占位类
-        importlib.import_module(f"src.models.{module_name}")
--- src/models/qwen3_next.py
+++
@@ -1,9 +0,0 @@
-from src.config.model_config import ModelConfig
-from src.models.base import BaseModel, ModelRegistry
-from src.server_args import ServerArgs
-
-
-@ModelRegistry.register("qwen3_next")
-class Qwen3Next(BaseModel):
-    def __init__(self, serverArgs: ServerArgs, config: ModelConfig):
-        super().__init__(serverArgs, config)
